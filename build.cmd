44b94a040a2d591e4b3160b5a0a905b204762e08
volth <volth@volth.com>
2020-10-25 19:41:52 -0700

    build with /MT so Nix binaries may be copied to older Windows

--- build.cmd
+++ build.cmd
@@ -19,13 +19,14 @@ set NIX_PATH=nixpkgs=C:\work\nixpkgs-windows
 rem for /f %%i in ('%OLDNIX%\bin\nix-build.exe --no-out-link -E "(import <nixpkgs> { }).msysPackages.coreutils"') do set COREUTILS=%%i
 rem echo COREUTILS=%COREUTILS%
 rem exit
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o x86_64-stdenv-cc -E "(import <nixpkgs> { }).stdenv.cc"') do set STDENV_CC=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o x86_64-boost     -E "(import <nixpkgs> { }).boost"'    ) do set BOOST=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o x86_64-openssl   -E "(import <nixpkgs> { }).openssl"'  ) do set OPENSSL=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o x86_64-xz        -E "(import <nixpkgs> { }).xz"'       ) do set XZ=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o x86_64-bzip2     -E "(import <nixpkgs> { }).bzip2"'    ) do set BZIP2=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o x86_64-curl      -E "(import <nixpkgs> { }).curl"'     ) do set CURL=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o x86_64-sqlite    -E "(import <nixpkgs> { }).sqlite"'   ) do set SQLITE=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o x86_64-stdenv-cc -E "with (import <nixpkgs> { }); stdenv.cc"'                                 ) do set STDENV_CC=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o x86_64-boost     -E "with (import <nixpkgs> { }); boost  .override{ staticRuntime=true; }"'   ) do set BOOST=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o x86_64-openssl   -E "with (import <nixpkgs> { }); openssl.override{ staticRuntime=true; }"'   ) do set OPENSSL=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o x86_64-zlib      -E "with (import <nixpkgs> { }); zlib   .override{ staticRuntime=true; }"'   ) do set ZLIB=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o x86_64-xz        -E "with (import <nixpkgs> { }); xz     .override{ staticRuntime=true; }"'   ) do set XZ=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o x86_64-bzip2     -E "with (import <nixpkgs> { }); bzip2  .override{ staticRuntime=true; }"'   ) do set BZIP2=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o x86_64-curl      -E "with (import <nixpkgs> { }); curl   .override{ staticRuntime=true; zlib=zlib.override{staticRuntime=true;}; openssl=openssl.override{staticRuntime=true;}; }"') do set CURL=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o x86_64-sqlite    -E "with (import <nixpkgs> { }); sqlite .override{ staticRuntime=true; }"'   ) do set SQLITE=%%i
 echo STDENV_CC=%STDENV_CC%
 echo BOOST=%BOOST%
 echo OPENSSL=%OPENSSL%
@@ -38,5 +39,5 @@ echo SQLITE=%SQLITE%
 %STDENV_CC%\bin\nmake -f Makefile.win STDENV_CC=%STDENV_CC% BOOST=%BOOST% OPENSSL=%OPENSSL% XZ=%XZ% BZIP2=%BZIP2% CURL=%CURL% SQLITE=%SQLITE% %*
 
 rem remove garbage like downloaded .iso files
-%OLDNIX%\bin\nix-store.exe --gc
-%OLDNIX%\bin\nix-store.exe --optimize
+rem %OLDNIX%\bin\nix-store.exe --gc
+rem %OLDNIX%\bin\nix-store.exe --optimize
--- build32.cmd
+++ build32.cmd
@@ -19,13 +19,15 @@ set NIX_PATH=nixpkgs=C:\work\nixpkgs-windows
 rem for /f %%i in ('%OLDNIX%\bin\nix-build.exe --no-out-link -E "(import <nixpkgs> { }).msysPackages.coreutils"') do set COREUTILS=%%i
 rem echo COREUTILS=%COREUTILS%
 rem exit
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o i686-stdenv-cc -E "(import <nixpkgs> { }).pkgsi686Windows.stdenv.cc"') do set STDENV_CC=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o i686-boost     -E "(import <nixpkgs> { }).pkgsi686Windows.boost"'    ) do set BOOST=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o i686-openssl   -E "(import <nixpkgs> { }).pkgsi686Windows.openssl"'  ) do set OPENSSL=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o i686-xz        -E "(import <nixpkgs> { }).pkgsi686Windows.xz"'       ) do set XZ=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o i686-bzip2     -E "(import <nixpkgs> { }).pkgsi686Windows.bzip2"'    ) do set BZIP2=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o i686-curl      -E "(import <nixpkgs> { }).pkgsi686Windows.curl"'     ) do set CURL=%%i
-for /f %%i in ('%OLDNIX%\bin\nix-build.exe -o i686-sqlite    -E "(import <nixpkgs> { }).pkgsi686Windows.sqlite"'   ) do set SQLITE=%%i
+
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o i686-stdenv-cc -E "with (import <nixpkgs> { }).pkgsi686Windows; stdenv.cc"'                                 ) do set STDENV_CC=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o i686-boost     -E "with (import <nixpkgs> { }).pkgsi686Windows; boost  .override{ staticRuntime=true; }"'   ) do set BOOST=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o i686-openssl   -E "with (import <nixpkgs> { }).pkgsi686Windows; openssl.override{ staticRuntime=true; }"'   ) do set OPENSSL=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o i686-zlib      -E "with (import <nixpkgs> { }).pkgsi686Windows; zlib   .override{ staticRuntime=true; }"'   ) do set ZLIB=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o i686-xz        -E "with (import <nixpkgs> { }).pkgsi686Windows; xz     .override{ staticRuntime=true; }"'   ) do set XZ=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o i686-bzip2     -E "with (import <nixpkgs> { }).pkgsi686Windows; bzip2  .override{ staticRuntime=true; }"'   ) do set BZIP2=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o i686-curl      -E "with (import <nixpkgs> { }).pkgsi686Windows; curl   .override{ staticRuntime=true; zlib=zlib.override{staticRuntime=true;}; openssl=openssl.override{staticRuntime=true;}; }"') do set CURL=%%i
+for /f %%i in ('%OLDNIX%\bin\nix-build.exe --keep-failed -o i686-sqlite    -E "with (import <nixpkgs> { }).pkgsi686Windows; sqlite .override{ staticRuntime=true; }"'   ) do set SQLITE=%%i
 echo STDENV_CC=%STDENV_CC%
 echo BOOST=%BOOST%
 echo OPENSSL=%OPENSSL%
@@ -38,6 +40,6 @@ echo SQLITE=%SQLITE%
 %STDENV_CC%\bin\nmake -f Makefile.win STDENV_CC=%STDENV_CC% BOOST=%BOOST% OPENSSL=%OPENSSL% XZ=%XZ% BZIP2=%BZIP2% CURL=%CURL% SQLITE=%SQLITE% %*
 
 rem remove garbage like downloaded .iso files
-%OLDNIX%\bin\nix-store.exe --gc
-%OLDNIX%\bin\nix-store.exe --optimize
+rem %OLDNIX%\bin\nix-store.exe --gc
+rem %OLDNIX%\bin\nix-store.exe --optimize
 
