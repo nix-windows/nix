64e009b13c8b1d40c29697ea452ba4a13113b6e9
John Ericson <John.Ericson@Obsidian.Systems>
2026-01-26 21:59:07 -0500

    wip libstore

--- src/libstore/include/nix/store/local-fs-store.hh
+++ src/libstore/include/nix/store/local-fs-store.hh
@@ -11,7 +11,7 @@ struct LocalFSStoreConfig : virtual StoreConfig
 {
 private:
     static Setting<std::optional<std::filesystem::path>>
-    makeRootDirSetting(LocalFSStoreConfig & self, std::optional<Path> defaultValue)
+    makeRootDirSetting(LocalFSStoreConfig & self, std::optional<std::filesystem::path> defaultValue)
     {
         return {
             &self,
@@ -41,13 +41,13 @@ private:
      * An indirection so that we don't need to refer to global settings
      * in headers.
      */
-    static Path getDefaultStateDir();
+    static std::filesystem::path getDefaultStateDir();
 
     /**
      * An indirection so that we don't need to refer to global settings
      * in headers.
      */
-    static Path getDefaultLogDir();
+    static std::filesystem::path getDefaultLogDir();
 
 public:
 
@@ -99,22 +99,22 @@ struct alignas(8) /* Work around ASAN failures on i686-linux. */
      * How the permanent GC root corresponding to this symlink is
      * managed is implementation-specific.
      */
-    virtual Path addPermRoot(const StorePath & storePath, const Path & gcRoot) = 0;
+    virtual std::filesystem::path addPermRoot(const StorePath & storePath, const std::filesystem::path & gcRoot) = 0;
 
     virtual std::filesystem::path getRealStoreDir()
     {
         return config.realStoreDir;
     }
 
-    Path toRealPath(const StorePath & storePath)
+    std::filesystem::path toRealPath(const StorePath & storePath)
     {
         return toRealPath(printStorePath(storePath));
     }
 
-    Path toRealPath(const Path & storePath)
+    std::filesystem::path toRealPath(const std::filesystem::path & storePath)
     {
         assert(isInStore(storePath));
-        return getRealStoreDir() + "/" + std::string(storePath, storeDir.size() + 1);
+        return getRealStoreDir() / std::filesystem::path(storePath.string().substr(storeDir.size() + 1));
     }
 
     std::optional<std::string> getBuildLogExact(const StorePath & path) override;
